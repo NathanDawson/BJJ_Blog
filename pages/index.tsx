3057f37801737d25ece1b50009edcc4cd7cdff5d
NathanDawson <nathan.dawson76@icloud.com>
2022-12-11 22:13:06 +0000

    Link Hygraph data to blog website with post info

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import {PostCard, Categories, PostWidget} from '../components';
+import {getPosts} from '../services'
 
-const posts = [
-  { title: 'Test 1', excerpt: 'This is a test' },
-  { title: 'Test 2', excerpt: 'This is also a test' },
-];
-
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -31,3 +27,11 @@ const Home: NextPage = () => {
   }
 
 export default Home
+
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {posts}
+  }
+}
