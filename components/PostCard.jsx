986904759650f2c63ae284feda0140262acac06f
NathanDawson <nathan.dawson76@icloud.com>
2022-12-19 13:28:22 +0000

    Add recent posts and categories

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -15,8 +15,8 @@ const PostCard = ( {post} ) => {
           {post.title}
         </Link>
       </h1>
-      <div className='block lg:flex text-center intems-center justify-center mb-8 w-full'>
-        <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
+      <div className='block lg:inline-block text-center items-center justify-center mb-8 w-full'>
+        <div className='flex items-center justify-center mb-8 lg:mb-6 w-full lg:w-auto mr-8 items-center'>
           <img 
             alt={post.author.name}
             height="30px"
@@ -24,7 +24,7 @@ const PostCard = ( {post} ) => {
             className='align-middle rounded-full'
             src={post.author.photo.url}
           />
-          <p className='inline align-middle text-gray-700 ml-2 text-lg'>{post.author.name}</p>
+          <p className='inline align-middle text-gray-700 ml-2 font-medium text-lg'>{post.author.name}</p>
           <div className='font-medium text-gray-700'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
